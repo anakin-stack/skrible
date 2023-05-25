9c807a4fb2e3c17b90dc773054769190d859944e
Ana <kamalkaushik56@gmail.com>
2023-05-25 11:58:15 +0530

    updated new url route to fetch the SVG data and use it to render

--- svr/app.js
+++ svr/app.js
@@ -4,20 +4,20 @@ const cors = require('cors');
 const fs = require('fs');
 const { createCanvas, loadImage } = require('canvas');
 
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Declare a variable to store svgMarkup
+app.locals.svgMarkup = '';
 
 // Endpoint to handle the POST request with JSON payload
 app.post('/api/paths', (req, res) => {
-  console.log("response, body, req, res////////////////")
-  const pathData = req.body
+  const pathData = req.body;
+
   // Function to convert path data to SVG
   function convertToSVG(pathData, width, height) {
     let svg = '<svg xmlns="http://www.w3.org/2000/svg" width="' + width + '" height="' + height + '">';
-
 
     // Loop through each path object and generate the corresponding SVG path element
     for (let i = 0; i < pathData.length; i++) {
@@ -33,19 +33,23 @@ app.post('/api/paths', (req, res) => {
   // Call the function to convert the path data to SVG markup
   const svgMarkup = convertToSVG(pathData, 600, 1000);
 
+  // Save svgMarkup to app.locals
+  app.locals.svgMarkup = svgMarkup;
+
   console.log(svgMarkup);
-
-
-  // Print the SVG string
-
 
   // Send a response back to the client
   res.json({ message: 'Paths received successfully' });
+});
+
+// Example endpoint to retrieve the svgMarkup from app.locals
+app.get('/api/svgMarkup', (req, res) => {
+  // Retrieve svgMarkup from app.locals
+  const svgMarkup = app.locals.svgMarkup;
+  res.send(svgMarkup);
 });
 
 // Start the server
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
 });
-
-// Example paths data
